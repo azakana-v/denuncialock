e9c8009a89142a45abdd367ca1226dea9c7429a5
FelipeProtazio <75281109+PedroTintino@users.noreply.github.com>
2024-07-30 22:55:32 -0300

    fix: container vertical align fixed

--- src/components/timeLine/styles.ts
+++ src/components/timeLine/styles.ts
@@ -5,6 +5,7 @@ export const DetailsContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
 `;
 export const Title = styled.h2`
   font-size: 2rem;
